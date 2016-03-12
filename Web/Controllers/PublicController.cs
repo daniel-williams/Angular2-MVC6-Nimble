7b786f50b181b56f7093a9e6c88d1b32e06cd260
daniel-williams <mail@danielwilliams.us>
2016-03-11 17:05:49 -0800

    Adding ASP.NET 5 MVC6 project.

---
+++ Web/Controllers/PublicController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace Web.Controllers
+{
+    public class PublicController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
