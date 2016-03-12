d71d9a4e856211e9e4c336dbfb34e9110f458988
daniel-williams <mail@danielwilliams.us>
2016-03-11 17:11:09 -0800

    Load styles as raw. Updates to integrate with MVC6 project. Serving in-memory bundles via webpack-dev-server.

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,22 +1,23 @@
 var path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var pkg = require('./package.json');
 
 var PROJECT_PATH = path.resolve(__dirname);
 var APP_PATH = path.resolve(PROJECT_PATH, 'app');
-var WEB_PATH = path.resolve(PROJECT_PATH, 'Web');
-var DIST_PATH = path.resolve(WEB_PATH, 'content/scripts');
+var WEB_PATH = path.resolve(PROJECT_PATH, 'Web/wwwroot');
+var DIST_PATH = path.resolve(WEB_PATH, 'content/bundles');
 
 
 module.exports = {
     entry: {
-        'app': path.join(APP_PATH, 'main.ts'),
-        'vendors': path.join(APP_PATH, 'vendors.ts'),
-        'style': path.join(APP_PATH, 'styles.scss')
+        'app': [
+          path.join(APP_PATH, 'main.ts'),
+          'webpack/hot/only-dev-server'
+        ],
+        'vendors': path.join(APP_PATH, 'vendors.ts')
     },
     output: {
         path: DIST_PATH,
-        filename: '[name]-[hash:6].js'
+        filename: '[name].bundle.js',
+        publicPath: 'http://localhost:8080/content/bundles/',
     },
     module: {
         loaders: [{
@@ -33,16 +34,11 @@ module.exports = {
             loader: 'raw'
         }, {
             test: /\.scss$/,
-            loader: "style!css!autoprefixer?browsers=last 2 versions!sass"
+            loader: "raw!autoprefixer?browsers=last 2 versions!sass"
         }],
         noParse: [/angular2\/bundles\/.+/],
     },
     resolve: {
         extensions: ['', '.ts', '.js', '.html', '.scss']
-    },
-    plugins: [new HtmlWebpackPlugin({
-        filename: 'index.html',
-        pkg: pkg,
-        template: path.join(WEB_PATH, 'index.html')
-    })]
+    }
 };
